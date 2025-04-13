8ede827af2f2ca6fc612428cd7fa4a19f4379b0c
bulv0620 <1241278815@qq.com>
2025-04-13 20:07:26 +0800

    移除 electron-store 依赖，更新主题管理逻辑，优化主题切换功能

--- src/main/events/index.ts
+++ src/main/events/index.ts
@@ -1,25 +1,16 @@
 import { BrowserWindow, dialog, ipcMain, nativeTheme, Tray } from 'electron'
-import ElectronStore from 'electron-store'
 import { updateTray } from '../utils/tray'
 
 export function createEventHandler({
   mainWindow,
-  store,
   tray,
 }: {
   mainWindow: BrowserWindow
-  store: ElectronStore
   tray: Tray
 }) {
-  // 获取APP当前主题模式
-  ipcMain.handle('current-theme', () => {
-    return nativeTheme.themeSource
-  })
-
   // 设置APP主题模式
   ipcMain.handle('switch-theme', (_, type: 'light' | 'dark' | 'system') => {
     nativeTheme.themeSource = type
-    store.set('theme', type)
   })
 
   // 设置APP语言更新tray
