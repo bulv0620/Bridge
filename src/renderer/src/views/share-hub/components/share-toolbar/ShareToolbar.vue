69fc514a33655be75ec79322859980af7077de01
bulv0620 <1241278815@qq.com>
2025-09-09 15:58:41 +0800

    修改文件分享页面

--- src/renderer/src/views/share-hub/components/share-toolbar/ShareToolbar.vue
+++ src/renderer/src/views/share-hub/components/share-toolbar/ShareToolbar.vue
@@ -1,51 +1,24 @@
 <script setup lang="ts">
 import { useSettingDrawer } from '@renderer/composables/share-hub/useSettingDrawer'
-import { TrashBinOutline } from '@vicons/ionicons5'
-import { FileDownloadOutlined, FileUploadOutlined } from '@vicons/material'
 
 const { settingsForm } = useSettingDrawer()
 </script>
 
 <template>
-  <n-space>
-    <CommonButton
-      :tooltip="$t('views.shareHub.upload')"
-      :icon="FileUploadOutlined"
-      :button-props="{ size: 'small', circle: true, type: 'primary' }"
-      placement="bottom"
-      :delay="500"
-    />
-    <CommonButton
-      :tooltip="$t('views.shareHub.download')"
-      :icon="FileDownloadOutlined"
-      :button-props="{ size: 'small', circle: true }"
-      placement="bottom"
-      :delay="500"
-    />
-    <CommonButton
-      :tooltip="$t('views.shareHub.remove')"
-      :icon="TrashBinOutline"
-      :button-props="{ size: 'small', circle: true }"
-      placement="bottom"
-      :delay="500"
-    />
-  </n-space>
-  <n-space>
-    <n-form-item
-      :label="$t('views.shareHub.enableSharing')"
-      :show-feedback="false"
-      label-placement="left"
-      size="small"
-    >
-      <n-switch v-model:value="settingsForm.enableSharing" />
-    </n-form-item>
-    <n-form-item
-      :label="$t('views.shareHub.showMyFiles')"
-      :show-feedback="false"
-      label-placement="left"
-      size="small"
-    >
-      <n-switch v-model:value="settingsForm.showMyFiles" />
-    </n-form-item>
-  </n-space>
+  <n-form-item
+    :label="$t('views.shareHub.enableSharing')"
+    :show-feedback="false"
+    label-placement="left"
+    size="small"
+  >
+    <n-switch v-model:value="settingsForm.enableSharing" />
+  </n-form-item>
+  <n-form-item
+    :label="$t('views.shareHub.showMyFiles')"
+    :show-feedback="false"
+    label-placement="left"
+    size="small"
+  >
+    <n-switch v-model:value="settingsForm.showMyFiles" />
+  </n-form-item>
 </template>
