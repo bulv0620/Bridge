c1ff29b8f815652e7e3f370bb76966086404f62c
bulv0620 <1241278815@qq.com>
2025-11-15 17:19:28 +0800

    去除tooltip

--- src/renderer/src/views/file-sync/components/plan-toolbar/PlanToolbar.vue
+++ src/renderer/src/views/file-sync/components/plan-toolbar/PlanToolbar.vue
@@ -9,60 +9,33 @@ const { resetPlan, savePlan, saveLoading, openPlanListModal, queryLoading } = us
 
 <template>
   <div class="plan-toolbar">
-    <el-tooltip
-      :content="$t('views.fileSync.newPlan')"
-      placement="bottom"
-      effect="light"
-      :show-after="500"
-      :hide-after="0"
+    <el-button
+      type="primary"
+      circle
+      :icon="DocumentOutline"
       :disabled="isSyncing || isComparing"
-    >
-      <el-button
-        type="primary"
-        circle
-        :icon="DocumentOutline"
-        :disabled="isSyncing || isComparing"
-        @click="resetPlan()"
-      ></el-button>
-    </el-tooltip>
+      @click="resetPlan()"
+    ></el-button>
 
-    <el-tooltip
-      :content="$t('views.fileSync.savePlan')"
-      placement="bottom"
-      effect="light"
-      :show-after="500"
-      :hide-after="0"
+    <el-button
+      circle
+      text
+      bg
+      :icon="SaveOutline"
       :disabled="isSyncing || isComparing"
-    >
-      <el-button
-        circle
-        text
-        bg
-        :icon="SaveOutline"
-        :disabled="isSyncing || isComparing"
-        :loading="saveLoading"
-        @click="savePlan()"
-      ></el-button>
-    </el-tooltip>
+      :loading="saveLoading"
+      @click="savePlan()"
+    ></el-button>
 
-    <el-tooltip
-      :content="$t('views.fileSync.savedPlans')"
-      placement="bottom"
-      effect="light"
-      :show-after="500"
-      :hide-after="0"
+    <el-button
+      circle
+      text
+      bg
+      :icon="FileTrayFullOutline"
       :disabled="isSyncing || isComparing"
-    >
-      <el-button
-        circle
-        text
-        bg
-        :icon="FileTrayFullOutline"
-        :disabled="isSyncing || isComparing"
-        :loading="queryLoading"
-        @click="openPlanListModal()"
-      ></el-button>
-    </el-tooltip>
+      :loading="queryLoading"
+      @click="openPlanListModal()"
+    ></el-button>
   </div>
 </template>
 
