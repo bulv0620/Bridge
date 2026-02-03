a584d51cd873c761e0031f5b3e29e1c2ac76ad5e
bulv0620 <bulv0620@gmail.com>
2026-02-03 08:40:43 +0800

    refactor: change deviceDiscovery to be nullable and initialize on demand

--- src/main/modules/share/index.ts
+++ src/main/modules/share/index.ts
@@ -1,11 +1,15 @@
 import { DeviceDiscovery } from './service/DeviceDiscovery'
 
-const deviceDiscovery = new DeviceDiscovery()
+let deviceDiscovery: DeviceDiscovery | null
 
 export function startUdpService() {
+  if (!deviceDiscovery) {
+    deviceDiscovery = new DeviceDiscovery()
+  }
   deviceDiscovery.start()
 }
 
 export function stopUdpService() {
+  if (!deviceDiscovery) return
   deviceDiscovery.stop()
 }
